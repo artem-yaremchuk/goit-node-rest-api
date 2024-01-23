9cb25b8d295694525a4884ea01e9db4fe2802ada
artem-yaremchuk <artem.yaremchuk@yahoo.com>
2024-01-23 18:05:34 +0200

    Feat: Add contactsControllers, validation, error handlers

---
+++ routes/contactsRouter.js
@@ -0,0 +1,22 @@
+import express from "express";
+import {
+  getAllContacts,
+  getOneContact,
+  deleteContact,
+  createContact,
+  updateOneContact,
+} from "../controllers/contactsControllers.js";
+
+const contactsRouter = express.Router();
+
+contactsRouter.get("/", getAllContacts);
+
+contactsRouter.get("/:id", getOneContact);
+
+contactsRouter.delete("/:id", deleteContact);
+
+contactsRouter.post("/", createContact);
+
+contactsRouter.put("/:id", updateOneContact);
+
+export default contactsRouter;

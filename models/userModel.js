b42865620e683fda04dabe06fc1ca559ae6e7ef9
artem-yaremchuk <artem.yaremchuk@yahoo.com>
2024-02-22 17:17:22 +0200

    Fix: Fix User model

--- models/userModel.js
+++ models/userModel.js
@@ -26,7 +26,6 @@ const userSchema = new Schema(
     },
     verificationToken: {
       type: String,
-      required: [true, "Verify token is required"],
     },
   },
   { versionKey: false, timestamps: true },
